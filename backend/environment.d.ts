d0e641be7095c6405a4d202e011cbf3353abe336
sazonov <fromlhz03@gmail.com>
2022-08-04 11:38:14 +0300

    add global environment types file

---
+++ backend/environment.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_TYPE: string;
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+      APP_PORT: string;
+      NODE_ENV: 'development' | 'production';
+    }
+  }
+}
+
+export {};
